f0ba9e5c509cf838bf15a86627f1b13fafa1d171
João Victor Fernandes <joaovictorfernandespinto@gmail.com>
2023-03-24 16:00:54 -0300

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+//mongodb password = osD0f4nCW3WFWhMF
+
+const mongoose = require('mongoose');
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.fyfaabs.mongodb.net/?retryWrites=true&w=majority`)
+    console.log("Conectou ao banco");
+
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+conn();
+
+module.exports = conn;
