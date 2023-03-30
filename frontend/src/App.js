5d8e8ab02a0b7bc96ad7da38fd30f1e1cab16a72
João Victor Fernandes <joaovictorfernandespinto@gmail.com>
2023-03-30 16:51:16 -0300

    adding-photo-and-editing-user
    Was added the page for editing user and insert a photo.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,11 +15,12 @@ import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import EditProfile from './pages/EditProfile/EditProfile';
+import Profile from './pages/Profile/Profile';
 
 function App() {
   const { auth, loading } = useAuth();
 
-  if(loading) {
+  if (loading) {
     return <p>Carregando...</p>
   }
 
@@ -29,18 +30,21 @@ function App() {
         <Navbar />
         <div className="container">
           <Routes>
-            <Route 
-              path="/" 
-              element={auth ? <Home /> : <Navigate to="/login"/>} />
-            <Route 
-              path="/profile" 
-              element={auth ? <EditProfile /> : <Navigate to="/login"/>} />
-            <Route 
-              path="/login" 
-              element={!auth ? <Login /> : <Navigate to="/"/>} />
-            <Route 
-              path="/register" 
-              element={!auth ? <Register /> : <Navigate to="/"/>} />
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />} />
+            <Route
+              path="/profile"
+              element={auth ? <EditProfile /> : <Navigate to="/login" />} />
+            <Route
+              path="/users/:id"
+              element={auth ? <Profile /> : <Navigate to="/login" />} />
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Navigate to="/" />} />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />} />
           </Routes>
         </div>
         <Footer />
--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -1,32 +1,118 @@
 import './EditProfile.css';
 
-const EditProfile = () => {
+import { uploads } from '../../utils/config';
 
-  const handleSubmit = e => {
+//hooks
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+//redux 
+import { profile, resetMessage, updateProfile } from '../../slices/userSlice';
+
+//components
+import Message from '../../components/Message';
+
+const EditProfile = () => {
+  const dispatch = useDispatch();
+
+  const { user, message, error, loading } = useSelector(state => state.user);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [profileImage, setProfileImage] = useState('');
+  const [bio, setBio] = useState('');
+  const [previewImage, setPreviewImage] = useState('');
+
+  //load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch])
+
+  //fill form with user data
+  useEffect(() => {
+    if(user) {
+      setName(user.name);
+      setEmail(user.email);
+      setBio(user.bio)
+    }
+  }, [user])
+
+  const handleSubmit = async e => {
     e.preventDefault();
+    //gather user data from states
+    const userData = { name }
+    if(profileImage) userData.profileImage = profileImage;
+    if(bio) userData.bio = bio;
+    if(password) userData.password = password;
+    //build form data
+    const formData = new FormData();
+    const userFormData = Object
+      .keys(userData)
+      .forEach(key => formData.append(key, userData[key]));
+    formData.append("user", userFormData);
+    await dispatch(updateProfile(userFormData));
+    setTimeout(() => {
+      dispatch(resetMessage())
+    }, 2000)
+  }
+
+  const handleFile = e => {
+    //image preview
+    const image = e.target.files[0];
+    setPreviewImage(image);
+    setProfileImage(image);
   }
 
   return (
     <div id="edit-profile">
       <h2>Edite seus dados</h2>
       <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre você...</p>
-      {{/* preview da imagem */}}
+      {(user.profileImage || previewImage) && (
+        <img 
+          className='profile-image'
+          src={
+            previewImage ? URL.createObjectURL(previewImage) : `${uploads}/users/${user.profileImage}`
+          }
+          alt={user.name}/>
+      )}
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Nome'/>
-        <input type="text" placeholder='E-mail' disabled/>
+        <input
+          type="text"
+          placeholder='Nome'
+          onChange={e => setName(e.target.value)}
+          value={name || ""} />
+        <input
+          type="text"
+          placeholder='E-mail'
+          disabled
+          value={email || ""} />
         <label>
           <span>Imagem do perfil: </span>
-          <input type="file" />
+          <input 
+            type="file" 
+            onChange={handleFile}/>
         </label>
         <label>
           <span>Bio: </span>
-          <input type="text" placeholder='Descrição do perfil.'/>
+          <input
+            type="text"
+            placeholder='Descrição do perfil.'
+            onChange={e => setBio(e.target.value)}
+            value={bio || ""} />
         </label>
         <label>
           <span>Quer alterar sua senha?: </span>
-          <input type="password" placeholder='Digite sua nova senha'/>
+          <input
+            type="password"
+            placeholder='Digite sua nova senha'
+            onChange={e => setPassword(e.target.value)}
+            value={password || ""} />
         </label>
-        <input type="submit" value="Alterar" />
+        {!loading && <input type="submit" value="Cadastrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="success" />}
       </form>
     </div>
   )
